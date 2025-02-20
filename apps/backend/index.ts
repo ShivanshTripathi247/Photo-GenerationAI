e7d8a33605da48ddb795823fae5ce311e0f9461d
ShivanshTripathi247 <shivansht06@gmail.com>
2025-02-20 20:14:22 +0530

    initial commit

---
+++ apps/backend/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+
+const PORT = process.env.PORT || 8080;
+
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Hello World");
+});
+
+app.post("/ai/generate", (req,res) => {
+
+})
+
+app.post("/pack/generate", (req,res) => {
+    
+})
+
+app.get("/pack/build", (req,res) => {
+    
+})
+
+app.get("/image", (req,res) => {
+    
+})
+
+app.listen(3000, () => {
+    console.log("Sever is running on port 8080 ");
+    
+})
