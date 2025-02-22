5eb02413775d2871b117cdb5047af66b40fe469c
ShivanshTripathi247 <shivansht06@gmail.com>
2025-02-22 22:44:22 +0530

    tailwind configured correctly

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,10 +4,12 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <div className="item-center justify-center flex text-white h-screen bg-black">
     <h1 className="text-3xl font-bold underline">
       Hello world!
       <Button variant={"outline"}>ksjkskslnl</Button>
     </h1>
+    </div>
 
   );
 }
