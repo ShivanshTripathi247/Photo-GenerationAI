fba0bcb4129d2e1c061cfc797f52f7d763dd0cb1
ShivanshTripathi247 <shivansht06@gmail.com>
2025-02-27 19:25:38 +0530

    landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Navbar from "@/components/ui/navbar";
 import { WavyBackground } from "@/components/ui/wavy-background";
 
 
@@ -7,7 +8,8 @@ import { WavyBackground } from "@/components/ui/wavy-background";
 export default function Home() {
   return (
     <div className="item-center justify-center flex text-white h-screen bg-black">
-      <WavyBackground className="max-w-4xl mx-auto pb-40">
+      <Navbar className="top-2" />
+      <WavyBackground className="max-w-4xl mx-auto pb-40 items-center justify-center flex flex-col">
       <p className="text-2xl md:text-4xl lg:text-7xl  font-bold inter-var text-center 
       bg-gradient-to-r from-red-400 to-fuchsia-100 bg-clip-text text-transparent">
         Welcome To PhotoGen-AI.
@@ -16,6 +18,10 @@ export default function Home() {
       <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
         Leverage the power of AI to generate you-specific Photos
       </p>
+      <Button className="mt-10 bg-red-300 hover:fuchsia-200">
+        View our Plans
+      </Button>
+      
     </WavyBackground>
     
     </div>
---
+++ apps/web/components/ui/navbar.tsx
@@ -0,0 +1,84 @@
+'use client'
+import { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./navbar-menu";
+import { cn } from "@/lib/utils";
+import { Search } from "lucide-react";
+import { PlaceholdersAndVanishInput } from "./placeholders-and-vanish-input";
+
+function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+    const placeholders = [
+        "What's the first rule of Fight Club?",
+        "Who is Tyler Durden?",
+        "Where is Andrew Laeddis Hiding?",
+        "Write a Javascript method to reverse a string",
+        "How to assemble your own PC?",
+      ];
+   
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+      };
+      const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("submitted");
+      };
+    return (
+        <div className={cn("fixed top-10 inset-x-0 max-w-4xl mx-auto z-50", className)}>
+
+        <Menu setActive={setActive}>
+            <PlaceholdersAndVanishInput
+                    placeholders={placeholders}
+                    onChange={handleChange}
+                    onSubmit={onSubmit}
+            />
+          <nav className="flex items-center gap-4">
+            <MenuItem setActive={setActive} active={active} item="Services">
+              <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+                <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+                <HoveredLink href="/branding">Branding</HoveredLink>
+              </div>
+            </MenuItem>
+            <MenuItem setActive={setActive} active={active} item="Products">
+              <div className="text-sm grid grid-cols-2 gap-10 p-4">
+                <ProductItem
+                  title="Algochurn"
+                  href="https://algochurn.com"
+                  src="https://assets.aceternity.com/demos/algochurn.webp"
+                  description="Prepare for tech interviews like never before."
+                />
+                <ProductItem
+                  title="Tailwind Master Kit"
+                  href="https://tailwindmasterkit.com"
+                  src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
+                  description="Production ready Tailwind css components for your next project"
+                />
+                <ProductItem
+                  title="Moonbeam"
+                  href="https://gomoonbeam.com"
+                  src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
+                  description="Never write from scratch again. Go from idea to blog in minutes."
+                />
+                <ProductItem
+                  title="Rogue"
+                  href="https://userogue.com"
+                  src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
+                  description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+                />
+              </div>
+            </MenuItem>
+            <MenuItem setActive={setActive} active={active} item="Pricing">
+              <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/hobby">Hobby</HoveredLink>
+                <HoveredLink href="/individual">Individual</HoveredLink>
+                <HoveredLink href="/team">Team</HoveredLink>
+                <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+              </div>
+            </MenuItem>
+          </nav>
+        </Menu>
+      </div>
+    );
+  }
+  export default Navbar;
